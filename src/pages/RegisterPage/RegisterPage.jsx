c0f8fb90b29a7b013c2fd4e5a3dbe1e31f9dfa91
unknown <golovinov817@gmail.com>
2025-06-12 15:21:26 +0200

    fix pages

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,8 @@
-import DocumentTitle from '../../components/DocumentTitle/DocumentTitle';
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 
 const RegisterPage = () => {
   return (
     <div>
-      <DocumentTitle>Registration</DocumentTitle>
       <RegistrationForm />
     </div>
   );
